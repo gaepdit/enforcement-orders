c4a00b94eaa8fbcdb503b5d84fbbd3c17f3c27a2
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-12-11 14:54:05 -0500

    Add concurrency token timestamp to Base Entity

--- Enfo.Domain/Entities/Base/BaseEntity.cs
+++ Enfo.Domain/Entities/Base/BaseEntity.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Enfo.Domain.Entities
 {
@@ -10,5 +12,12 @@ namespace Enfo.Domain.Entities
         public Guid? UpdatedById { get; set; }
         public DateTimeOffset? CreatedDate { get; set; }
         public Guid? CreatedById { get; set; }
+
+        /// <summary>
+        /// See https://docs.microsoft.com/en-us/ef/core/modeling/concurrency
+        /// </summary>
+        [Timestamp]
+        [SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "Used for concurrency token")]
+        public byte[] Timestamp { get; set; }
     }
 }
