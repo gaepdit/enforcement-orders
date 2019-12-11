a1c4f30556f69f2f454f0185500f015a4da55a8d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-12-11 16:27:00 -0500

    Make Program class and Startup methods static

--- Enfo.API/Startup.cs
+++ Enfo.API/Startup.cs
@@ -19,7 +19,7 @@ namespace Enfo.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
 
@@ -54,7 +54,7 @@ namespace Enfo.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(
+        public static void Configure(
             IApplicationBuilder app,
             IWebHostEnvironment env,
             EnfoDbContext context)
