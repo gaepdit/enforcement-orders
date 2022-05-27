f6219c315c57a9eb7ec37919ab2a69b93b94799d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-05-27 12:52:45 -0400

    Add EF migration for Attachments
    From Infrastructure project:
    `dotnet ef migrations add AddAttachments --msbuildprojectextensionspath ..\..\artifacts\Infrastructure\obj\`

--- src/Infrastructure/Migrations/20220527164833_AddAttachments.Designer.cs
+++ src/Infrastructure/Migrations/20220527164833_AddAttachments.Designer.cs
@@ -4,6 +4,7 @@ using Enfo.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Enfo.Infrastructure.Migrations
 {
     [DbContext(typeof(EnfoDbContext))]
-    partial class EnfoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220527164833_AddAttachments")]
+    partial class AddAttachments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
