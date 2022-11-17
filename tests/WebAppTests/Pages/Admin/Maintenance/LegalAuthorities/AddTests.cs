73cb55c80bc5ce0d945dee99a89bbcd334f378b5
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-11-17 10:32:48 -0500

    Use manual validation for Legal Authority

--- tests/WebAppTests/Pages/Admin/Maintenance/LegalAuthorities/AddTests.cs
+++ tests/WebAppTests/Pages/Admin/Maintenance/LegalAuthorities/AddTests.cs
@@ -5,12 +5,15 @@ using Enfo.WebApp.Pages.Admin.Maintenance.LegalAuthorities;
 using Enfo.WebApp.Platform.RazorHelpers;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Moq;
 using NUnit.Framework;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EnfoTests.WebApp.Pages.Admin.Maintenance.LegalAuthorities;
@@ -27,13 +30,16 @@ public class AddTests
             .ReturnsAsync(false);
         repo.Setup(l => l.CreateAsync(It.IsAny<LegalAuthorityCommand>()))
             .ReturnsAsync(1);
+        var validator = new Mock<IValidator<LegalAuthorityCommand>>();
+        validator.Setup(l => l.ValidateAsync(It.IsAny<LegalAuthorityCommand>(), CancellationToken.None))
+            .ReturnsAsync(new ValidationResult());
 
         // Initialize Page TempData
         var httpContext = new DefaultHttpContext();
         var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
         var page = new Add { TempData = tempData, Item = item };
 
-        var result = await page.OnPostAsync(repo.Object);
+        var result = await page.OnPostAsync(repo.Object, validator.Object);
 
         var expected = new DisplayMessage(Context.Success,
             $"{item.AuthorityName} successfully added.");
@@ -51,10 +57,13 @@ public class AddTests
     public async Task OnPost_GivenModelError_ReturnsPageWithModelError()
     {
         var repo = new Mock<ILegalAuthorityRepository> { DefaultValue = DefaultValue.Mock };
+        var validator = new Mock<IValidator<LegalAuthorityCommand>>();
+        validator.Setup(l => l.ValidateAsync(It.IsAny<LegalAuthorityCommand>(), CancellationToken.None))
+            .ReturnsAsync(new ValidationResult());
         var page = new Add { Item = new LegalAuthorityCommand() };
         page.ModelState.AddModelError("key", "message");
 
-        var result = await page.OnPostAsync(repo.Object);
+        var result = await page.OnPostAsync(repo.Object, validator.Object);
 
         using (new AssertionScope())
         {
