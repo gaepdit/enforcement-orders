69bbe5a6f7f247eb7dd7007bd2bbf20dff490307
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2023-02-02 09:32:53 -0500

    Migrate to Microsoft Identity Web for authentication (#97)
    * Migrate to Microsoft Identity Web from AzureAD.UI for authentication
    
    * Remove subject ID from app user model and add EF migration
    
    * Minor identity code cleanup

--- src/Domain/Users/Entities/ApplicationUser.cs
+++ src/Domain/Users/Entities/ApplicationUser.cs
@@ -2,37 +2,33 @@
 
 namespace Enfo.Domain.Users.Entities;
 
-// Add profile data for application users by adding properties to the 
-// ApplicationUser class
-
+// Add profile data for application users by adding properties to the ApplicationUser class.
+// (IdentityUser already includes Id, Email, and UserName properties.)
 public class ApplicationUser : IdentityUser<Guid>
 {
     /// <summary>
-    /// The URI for a claim that specifies the given name of an entity, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname
+    /// A claim that specifies the given name of an entity, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname
     /// </summary>
     [ProtectedPersonalData]
     [StringLength(150)]
     public string GivenName { get; set; }
 
     /// <summary>
-    /// The URI for a claim that specifies the surname of an entity, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname
+    /// A claim that specifies the surname of an entity, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname
     /// </summary>
     [ProtectedPersonalData]
     [StringLength(150)]
     public string FamilyName { get; set; }
 
     /// <summary>
-    /// Equivalent to ExternalLoginInfo ProviderKey:
-    /// "The unique identifier for this user provided by the login provider."
-    /// Also ClaimTypes.NameIdentifier:
-    /// "The URI for a claim that specifies the name of an entity, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier."
-    /// </summary>
-    [PersonalData]
-    public string SubjectId { get; set; }
-
-    /// <summary>
-    /// Equivalent to ClaimTypes.ObjectId:
-    /// "Old Object Id claim: http://schemas.microsoft.com/identity/claims/objectidentifier."
+    /// "oid: The object identifier for the user in Azure AD. This value is the immutable and non-reusable identifier
+    /// of the user. Use this value, not email, as a unique identifier for users; email addresses can change.
+    /// If you use the Azure AD Graph API in your app, object ID is that value used to query profile information."
+    /// https://learn.microsoft.com/en-us/azure/architecture/multitenant-identity/claims
+    ///
+    /// In ASP.NET Core, the OpenID Connect middleware converts some of the claim types when it populates the
+    /// Claims collection for the user principal:
+    /// oid > http://schemas.microsoft.com/identity/claims/objectidentifier
     /// </summary>
     [PersonalData]
     public string ObjectId { get; set; }
