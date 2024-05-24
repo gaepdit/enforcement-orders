43fd69bc4b43bd00ce702dbea0e10f0cd7bc9bbf
Caleb Nguyen <caleb.nguyen@dnr.ga.gov>
2024-05-24 14:39:10 -0400

    Added "UserDomainValidation" function

---
+++ src/Domain/Users/Entities/UserDomainValidation.cs
@@ -0,0 +1,8 @@
+﻿namespace Enfo.Domain.Users.Entities
+{
+    public static class UserDomainValidation
+    {
+        public static bool isValidEmailDomain(this string email) =>
+            email.EndsWith("@dnr.ga.gov", StringComparison.CurrentCultureIgnoreCase);
+    }
+}
