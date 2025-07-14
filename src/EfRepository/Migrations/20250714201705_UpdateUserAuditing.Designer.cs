758f97b770281ddfb77542920ce7ff05ad64b78a
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-07-14 16:19:09 -0400

    Add EF migration for updated user metadata columns

--- src/EfRepository/Migrations/20250714201705_UpdateUserAuditing.Designer.cs
+++ src/EfRepository/Migrations/20250714201705_UpdateUserAuditing.Designer.cs
@@ -4,6 +4,7 @@ using Enfo.EfRepository.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Enfo.EfRepository.Migrations
 {
     [DbContext(typeof(EnfoDbContext))]
-    partial class EnfoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250714201705_UpdateUserAuditing")]
+    partial class UpdateUserAuditing
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/EfRepository/Migrations/20250714201705_UpdateUserAuditing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Enfo.EfRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserAuditing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ObjectId",
+                table: "AppUsers");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "AccountCreatedAt",
+                table: "AppUsers",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "AccountUpdatedAt",
+                table: "AppUsers",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountCreatedAt",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AccountUpdatedAt",
+                table: "AppUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ObjectId",
+                table: "AppUsers",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true);
+        }
+    }
+}
