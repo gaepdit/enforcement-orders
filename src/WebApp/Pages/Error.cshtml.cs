23ea943a0ac9e00957092558327a6045c257d5ab
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-07-14 16:08:13 -0400

    Add manual errors for testing

--- src/WebApp/Pages/Error.cshtml.cs
+++ src/WebApp/Pages/Error.cshtml.cs
@@ -7,10 +7,33 @@ namespace Enfo.WebApp.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [AllowAnonymous]
-public class ErrorModel : PageModel
+public class ErrorModel(ILogger<ErrorModel> logger) : PageModel
 {
     public string RequestId { get; private set; }
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     public void OnGet() => RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+    public void OnGetHandledError()
+    {
+        try
+        {
+            throw new NotImplementedException("Testing handled exception.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public void OnGetUnhandledError()
+    {
+        throw new NotImplementedException("Testing unhandled exception.");
+    }
+
+    public void OnGetLoggedError()
+    {
+        logger.LogError("Testing logged error.");
+    }
 }
