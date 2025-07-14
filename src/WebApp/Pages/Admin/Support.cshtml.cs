f017ae0467f38e041319cc584b0b0ef46b53fde9
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-07-14 16:07:55 -0400

    Update settings

--- src/WebApp/Pages/Admin/Support.cshtml.cs
+++ src/WebApp/Pages/Admin/Support.cshtml.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Enfo.WebApp.Pages.Admin;
@@ -7,8 +6,8 @@ namespace Enfo.WebApp.Pages.Admin;
 [Authorize]
 public class Support : PageModel
 {
-    public string SupportEmail { get; private set; }
-
-    public void OnGet([FromServices] IConfiguration configuration) =>
-        SupportEmail = configuration["SupportEmail"];
+    public void OnGet()
+    {
+        // Method intentionally left empty.
+    }
 }
