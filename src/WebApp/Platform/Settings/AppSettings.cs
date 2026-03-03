2e9b76442d008385160fef356c49987d4ff99f5c
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2026-03-03 15:08:23 -0500

    Add Datadog front-end monitoring (#168)

--- src/WebApp/Platform/Settings/AppSettings.cs
+++ src/WebApp/Platform/Settings/AppSettings.cs
@@ -5,14 +5,26 @@ namespace Enfo.WebApp.Platform.Settings;
 internal static partial class AppSettings
 {
     public static string Version { get; private set; }
+    public static string SimpleVersion => Version?.Split('+')[0];
+
+    public static string Env { get; } = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "unknown";
+    public static string ShortEnv => Env switch { "Production" => "prod", "Staging" => "uat", _ => "dev" };
+
     public static Support Support { get; } = new();
     public static EntraIdPhaseOut EntraIdPhaseOut { get; } = new();
     public static Raygun RaygunSettings { get; } = new();
+    public static DataDog DataDogSettings { get; } = new();
     public static string OrgNotificationsApiUrl { get; private set; }
 
     public record Raygun
     {
         public string ApiKey { get; [UsedImplicitly] init; }
+    }
+
+    public record DataDog
+    {
+        public string ClientToken { get; [UsedImplicitly] init; }
+        public string ApplicationId { get; [UsedImplicitly] init; }
     }
 }
 
