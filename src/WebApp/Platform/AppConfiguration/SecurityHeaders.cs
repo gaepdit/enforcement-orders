e5681625ef6c7fdee3ffa2bbcf4701a09712c236
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-03-03 16:20:04 -0500

    Update CSP policy for Datadog

--- src/WebApp/Platform/AppConfiguration/SecurityHeaders.cs
+++ src/WebApp/Platform/AppConfiguration/SecurityHeaders.cs
@@ -4,8 +4,11 @@ namespace Enfo.WebApp.Platform.AppConfiguration;
 
 internal static class SecurityHeaders
 {
-    private static readonly string ReportUri =
-        $"https://report-to-api.raygun.com/reports?apikey={AppSettings.RaygunSettings.ApiKey}";
+    private static readonly string DatadogReportUri =
+        $"https://browser-intake-us3-datadoghq.com/api/v2/logs?" +
+        $"dd-api-key={AppSettings.DataDogSettings.ClientToken}" +
+        $"&dd-evp-origin=content-security-policy&ddsource=csp-report" +
+        $"&ddtags=service%3Aenfo%2Cenv%3A{AppSettings.ShortEnv}%2Cversion%3A{AppSettings.SimpleVersion}";
 
     internal static void AddSecurityHeaderPolicies(this HeaderPolicyCollection policies)
     {
@@ -18,8 +21,8 @@ internal static class SecurityHeaders
         policies.AddCrossOriginEmbedderPolicy(builder => builder.Credentialless());
         policies.AddCrossOriginResourcePolicy(builder => builder.SameSite());
 
-        if (string.IsNullOrEmpty(AppSettings.RaygunSettings.ApiKey)) return;
-        policies.AddReportingEndpoints(builder => builder.AddEndpoint("csp-endpoint", ReportUri));
+        if (string.IsNullOrEmpty(AppSettings.DataDogSettings.ClientToken)) return;
+        policies.AddReportingEndpoints(builder => builder.AddEndpoint("csp-endpoint", DatadogReportUri));
     }
 
 #pragma warning disable S1075 // "URIs should not be hardcoded"
@@ -29,6 +32,7 @@ internal static class SecurityHeaders
         builder.AddBaseUri().None();
         builder.AddObjectSrc().None();
         builder.AddScriptSrc().Self()
+            .From("https://www.datadoghq-browser-agent.com/us3/v6/")
             .From("https://cdn.raygun.io/raygun4js/raygun.min.js")
             .WithHash256("Tui7QoFlnLXkJCSl1/JvEZdIXTmBttnWNxzJpXomQjg=") // Swagger UI inline script
             .WithHash256("ZfBxknfwMkoMSoaip4gXIEEtJwKW2s2WlmmCB03P704=") // Swagger UI inline script
@@ -40,6 +44,7 @@ internal static class SecurityHeaders
             .ReportSample();
         builder.AddImgSrc().Self().Data();
         builder.AddConnectSrc().Self()
+            .From("https://browser-intake-us3-datadoghq.com")
             .From("https://api.raygun.com")
             .From("https://api.raygun.io");
         builder.AddFontSrc().Self();
@@ -48,9 +53,8 @@ internal static class SecurityHeaders
             .From("https://login.microsoftonline.com");
         builder.AddManifestSrc().Self();
         builder.AddFrameAncestors().None();
-
-        if (string.IsNullOrEmpty(AppSettings.RaygunSettings.ApiKey)) return;
-        builder.AddReportUri().To(ReportUri);
+        builder.AddWorkerSrc()
+            .From("https://www.datadoghq-browser-agent.com/us3/v6/");
         builder.AddReportTo("csp-endpoint");
     }
 #pragma warning restore S1075
